c285ab0e7dcaa0a5c0ce50d2ffe2b93bc32bd526
Tim Goodwin <timg.goodwin@gmail.com>
2024-03-01 11:13:57 -0800

    increase sleep time of bootstrap script

--- tracey/bootstrap.sh
+++ tracey/bootstrap.sh
@@ -6,13 +6,13 @@ set -euo
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.14.3/cert-manager.yaml
 
 # wait for the cert manager pods to be ready before proceeding
-sleep 5
+sleep 10
 
 # install tempo operator controller
 kubectl apply -f https://github.com/grafana/tempo-operator/releases/latest/download/tempo-operator.yaml
 
 # wait for the tempo operator pods to be ready before proceeding
-sleep 5
+sleep 10
 
 # set up object storage
 kubectl apply -f https://raw.githubusercontent.com/grafana/tempo-operator/41d57e9ec1f78bc9789d3cf55241b2fed2faa269/minio.yaml
@@ -21,7 +21,7 @@ kubectl apply -f https://raw.githubusercontent.com/grafana/tempo-operator/41d57e
 kubectl apply -f tempo-storage-secret.yaml
 
 # wait for the minio pods to be ready before proceeding
-sleep 5
+sleep 10
 
 # install tempo stack custom resource
 kubectl apply -f tempo.yaml
