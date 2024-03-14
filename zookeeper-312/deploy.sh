347ef66fe962b5174c5a14572d43269e6b66db6a
Tim Goodwin <timg.goodwin@gmail.com>
2024-03-14 12:49:34 -0400

    increase sleep time

--- zookeeper-312/deploy.sh
+++ zookeeper-312/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -x
 
-kubectl create -f crds
-kubectl create -f default_ns/rbac.yaml
-kubectl create -f default_ns/operator.yaml
+NAMESPACE=default
+
+kubectl create --namespace $NAMESPACE -f crds
+kubectl create --namespace $NAMESPACE -f default_ns/rbac.yaml
+kubectl create --namespace $NAMESPACE -f default_ns/operator.yaml
